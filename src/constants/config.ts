53fcf3630411c78da5c4bc4dbf73262432234167
jacquibattin <80225550+jacquibattin@users.noreply.github.com>
2022-11-02 16:37:36 +0800

    fix wangka maya link

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,8 +4,8 @@ export const CONFIG = {
   wordLength: 6, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
   author: 'Jacqui Battin', // Put your name here so people know who made this Wordle!
   authorWebsite: '', // Put a link to your website or social media here
-  wordListSource: 'Martu Wangka Dictionary, 2006, an electronic dictionary published by Wangka Maya Pilbara Aboriginal Language Centre, which compiles data from Marsh (1992) and Hansen (2004)', // Describe the source material for your words here
-  wordListSourceLink: 'wangkamaya.org.au', // Put a link to the source material for your words here
+  wordListSource: 'Martu Wangka Dictionary, 2006, an electronic dictionary published by Wangka Maya Pilbara Aboriginal Language Centre', // Describe the source material for your words here
+  wordListSourceLink: 'http://wangkamaya.org.au', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
